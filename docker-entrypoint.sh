5bdd61f084fddbc4d2191273ac8b2f0494b959e7
sighttviewliu <denchance@gmail.com>
2019-05-24 12:46:19 +0800

    title: centos74ka comment: Dockerfile

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,11 +8,12 @@ shopt -s nullglob
 #    echo "/usr/sbin/init"
 #fi
 
+sh -c "sysctl -p"
 #sh -c "ipvsadm"
 #exec "$@"
-#exec "/usr/sbin/init"
+exec "/usr/sbin/init"
 #sh -c "/usr/sbin/init"
 #source "/usr/sbin/init"
 
 ## keep running
-echo "..." > file.txt && tail -f file.txt
+#echo "..." > file.txt && tail -f file.txt
--- run.sh
+++ run.sh
@@ -17,4 +17,4 @@ docker -D run -it --rm --name centoska74 --privileged -p 18880:80 -v $(pwd)/keep
 docker -D run -it --rm --name centoska74 --privileged -p 18880:80 -v $(pwd)/keepalived.conf:/etc/keepalived/keepalived.conf -v $(pwd)/sysctl.conf:/etc/sysctl.conf -v $(pwd)/ipvsadm:/etc/sysconfig/ipvsadm centoska:7.4 /usr/sbin/init
 
 ## For testing
-docker -D run -it --rm --name centoska74 --privileged -p 18880:80 -v $(pwd)/keepalived.conf:/etc/keepalived/keepalived.conf -v $(pwd)/sysctl.conf:/etc/sysctl.conf -v $(pwd)/ipvsadm:/etc/sysconfig/ipvsadm centoska:7.4
+docker -D run -it --rm --name centoska74 --privileged -p 18880:80 -v $(pwd)/keepalived.conf:/etc/keepalived/keepalived.conf -v $(pwd)/sysctl.conf:/etc/sysctl.conf -v $(pwd)/ipvsadm:/etc/sysconfig/ipvsadm -v $(pwd)/config:/etc/selinux/config centoska:7.4
