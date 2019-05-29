658cc7a4ed01cc2bd04c0e396ca08b7f38aa0d70
sighttviewliu <denchance@gmail.com>
2019-05-29 20:27:14 +0800

    title: centos74ka comment: networking job

--- run.sh
+++ run.sh
@@ -27,3 +27,34 @@ docker -D run -it --rm --name centoska74 --privileged -p 18880:80 -v $(pwd)/keep
 
 ## For testing
 docker -D run -it --rm --name centoska74 --privileged -p 38080:80 -v $(pwd)/keepalived.conf.master:/etc/keepalived/keepalived.conf -v $(pwd)/sysctl.conf:/etc/sysctl.conf -v $(pwd)/ipvsadm_master:/etc/sysconfig/ipvsadm -v $(pwd)/config:/etc/selinux/config centoska:7.4
+
+## For testing
+docker -D run -it --rm --name centoska74 --privileged --network=backend -p 38080:80 -v $(pwd)/keepalived.conf.master:/etc/keepalived/keepalived.conf -v $(pwd)/sysctl.conf:/etc/sysctl.conf -v $(pwd)/ipvsadm_master:/etc/sysconfig/ipvsadm -v $(pwd)/config:/etc/selinux/config centoska:7.4
+
+## For testing
+docker network create --driver=bridge --subnet=10.48.80.0/24 backend
+docker -D run -it --rm --name centoska74_0 --privileged --network=backend -p 38080:80 -v $(pwd)/keepalived.conf.master:/etc/keepalived/keepalived.conf -v $(pwd)/sysctl.conf:/etc/sysctl.conf -v $(pwd)/ipvsadm_master:/etc/sysconfig/ipvsadm -v $(pwd)/config:/etc/selinux/config centoska:7.4
+docker -D run -it --rm --name centoska74_1 --privileged --network=backend -p 38081:80 -v $(pwd)/keepalived.conf.backup:/etc/keepalived/keepalived.conf -v $(pwd)/sysctl.conf:/etc/sysctl.conf -v $(pwd)/ipvsadm_backup:/etc/sysconfig/ipvsadm -v $(pwd)/config:/etc/selinux/config centoska:7.4
+
+## For testing
+docker network create --driver=bridge --subnet=10.48.80.0/24 backend
+docker network create --driver=bridge --subnet=192.168.100.0/24 frontend
+#### Option 1
+docker -D run -it --rm --name centoska74 --privileged --network=backend -p 38080:80 -v $(pwd)/keepalived.conf.master:/etc/keepalived/keepalived.conf -v $(pwd)/sysctl.conf:/etc/sysctl.conf -v $(pwd)/ipvsadm_master:/etc/sysconfig/ipvsadm -v $(pwd)/config:/etc/selinux/config centoska:7.4
+docker network connect frontend centoska74
+#### Option 2
+docker -D create -it --rm --name centoska74 --privileged --network=backend -p 38080:80 -v $(pwd)/keepalived.conf.master:/etc/keepalived/keepalived.conf -v $(pwd)/sysctl.conf:/etc/sysctl.conf -v $(pwd)/ipvsadm_master:/etc/sysconfig/ipvsadm -v $(pwd)/config:/etc/selinux/config centoska:7.4
+docker network connect frontend centoska74
+docker start centoska74
+
+## For testing
+docker network create --driver=bridge --subnet=10.48.80.0/24 backend
+docker network create --driver=bridge --subnet=192.168.100.0/24 frontend
+#### Option 1
+docker -D run -it --rm --name centoska74 --privileged --network=backend --ip 10.48.80.11 -p 38080:80 -v $(pwd)/keepalived.conf.master:/etc/keepalived/keepalived.conf -v $(pwd)/sysctl.conf:/etc/sysctl.conf -v $(pwd)/ipvsadm_master:/etc/sysconfig/ipvsadm -v $(pwd)/config:/etc/selinux/config centoska:7.4
+docker network connect --ip 192.168.100.222 frontend centoska74
+#### Option 2
+docker -D create -it --rm --name centoska74 --privileged --network=backend --ip 10.48.80.11 -p 38080:80 -v $(pwd)/keepalived.conf.master:/etc/keepalived/keepalived.conf -v $(pwd)/sysctl.conf:/etc/sysctl.conf -v $(pwd)/ipvsadm_master:/etc/sysconfig/ipvsadm -v $(pwd)/config:/etc/selinux/config centoska:7.4
+docker network connect --ip 192.168.100.222 frontend centoska74
+docker start centoska74
+
