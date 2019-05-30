59546bbd755feded434a866d2fd9eea7ac50dc32
sighttviewliu <denchance@gmail.com>
2019-05-30 15:58:12 +0800

    title: centos74ka comment: testing

--- run.sh
+++ run.sh
@@ -58,3 +58,14 @@ docker -D create -it --rm --name centoska74 --privileged --network=backend --ip 
 docker network connect --ip 192.168.100.222 frontend centoska74
 docker start centoska74
 
+## For testing
+docker network create --driver=bridge --subnet=10.48.80.0/24 us
+docker network create --driver=bridge --subnet=192.168.100.0/24 is
+## halb 0
+docker -D create -it --rm --name centoska74_0 --privileged --network=us --ip 10.48.80.30 -p 38080:80 -v $(pwd)/keepalived.conf.master:/etc/keepalived/keepalived.conf -v $(pwd)/sysctl.conf:/etc/sysctl.conf -v $(pwd)/ipvsadm_master:/etc/sysconfig/ipvsadm -v $(pwd)/config:/etc/selinux/config centoska:7.4
+docker network connect --ip 192.168.100.130 is centoska74_0
+docker start centoska74_0
+## halb 1 
+docker -D create -it --rm --name centoska74_1 --privileged --network=us --ip 10.48.80.31 -p 38081:80 -v $(pwd)/keepalived.conf.backup:/etc/keepalived/keepalived.conf -v $(pwd)/sysctl.conf:/etc/sysctl.conf -v $(pwd)/ipvsadm_backup:/etc/sysconfig/ipvsadm -v $(pwd)/config:/etc/selinux/config centoska:7.4
+docker network connect --ip 192.168.100.131 is centoska74_1
+docker start centoska74_1
